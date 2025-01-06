4768c0a1bc15e46d62912778d0a887ab6aafbceb
MORAGE6607 MORAGE6607 <morageluqman@gmail.com>
2025-01-06 04:54:39 +0000

    feat: added Fluorine and Neon to the database

--- periodic_table.sql
+++ periodic_table.sql
@@ -125,6 +125,8 @@ INSERT INTO public.elements VALUES (6, 'C', 'Carbon');
 INSERT INTO public.elements VALUES (7, 'N', 'Nitrogen');
 INSERT INTO public.elements VALUES (8, 'O', 'Oxygen');
 INSERT INTO public.elements VALUES (1000, 'Mt', 'moTanium');
+INSERT INTO public.elements VALUES (9, 'F', 'Fluorine');
+INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 
 
 --
@@ -140,6 +142,8 @@ INSERT INTO public.properties VALUES (6, 12.011000, 3550, 4027, 2);
 INSERT INTO public.properties VALUES (7, 14.007000, -210.1, -195.8, 2);
 INSERT INTO public.properties VALUES (8, 15.999000, -218, -183, 2);
 INSERT INTO public.properties VALUES (1000, 1.000000, 10, 100, 3);
+INSERT INTO public.properties VALUES (9, 18.998000, -220, -188.1, 2);
+INSERT INTO public.properties VALUES (10, 20.180000, -248.6, -246.1, 2);
 
 
 --
