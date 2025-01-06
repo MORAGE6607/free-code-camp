877eec05b7aa53d49cdb6d7bcd8889199d008c09
MORAGE6607 MORAGE6607 <morageluqman@gmail.com>
2025-01-06 04:54:56 +0000

    fix: removed invalid element with atomic_number 1000

--- periodic_table.sql
+++ periodic_table.sql
@@ -124,7 +124,6 @@ INSERT INTO public.elements VALUES (5, 'B', 'Boron');
 INSERT INTO public.elements VALUES (6, 'C', 'Carbon');
 INSERT INTO public.elements VALUES (7, 'N', 'Nitrogen');
 INSERT INTO public.elements VALUES (8, 'O', 'Oxygen');
-INSERT INTO public.elements VALUES (1000, 'Mt', 'moTanium');
 INSERT INTO public.elements VALUES (9, 'F', 'Fluorine');
 INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 
@@ -141,7 +140,6 @@ INSERT INTO public.properties VALUES (5, 10.810000, 2075, 4000, 3);
 INSERT INTO public.properties VALUES (6, 12.011000, 3550, 4027, 2);
 INSERT INTO public.properties VALUES (7, 14.007000, -210.1, -195.8, 2);
 INSERT INTO public.properties VALUES (8, 15.999000, -218, -183, 2);
-INSERT INTO public.properties VALUES (1000, 1.000000, 10, 100, 3);
 INSERT INTO public.properties VALUES (9, 18.998000, -220, -188.1, 2);
 INSERT INTO public.properties VALUES (10, 20.180000, -248.6, -246.1, 2);
 
