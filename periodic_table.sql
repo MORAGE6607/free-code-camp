2e8e6a4c25edd26ba37544c2b046b03c12880abe
MORAGE6607 MORAGE6607 <morageluqman@gmail.com>
2025-01-06 04:53:32 +0000

    fix: linked properties table to types table and removed type column

--- periodic_table.sql
+++ periodic_table.sql
@@ -62,10 +62,10 @@ ALTER TABLE public.elements OWNER TO freecodecamp;
 
 CREATE TABLE public.properties (
     atomic_number integer NOT NULL,
-    type character varying(30),
     atomic_mass numeric(9,6) NOT NULL,
     melting_point_celsius numeric NOT NULL,
-    boiling_point_celsius numeric NOT NULL
+    boiling_point_celsius numeric NOT NULL,
+    type_id integer NOT NULL
 );
 
 
@@ -131,15 +131,15 @@ INSERT INTO public.elements VALUES (1000, 'mT', 'moTanium');
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (1, 'nonmetal', 1.008000, -259.1, -252.9);
-INSERT INTO public.properties VALUES (2, 'nonmetal', 4.002600, -272.2, -269);
-INSERT INTO public.properties VALUES (3, 'metal', 6.940000, 180.54, 1342);
-INSERT INTO public.properties VALUES (4, 'metal', 9.012200, 1287, 2470);
-INSERT INTO public.properties VALUES (5, 'metalloid', 10.810000, 2075, 4000);
-INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011000, 3550, 4027);
-INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007000, -210.1, -195.8);
-INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999000, -218, -183);
-INSERT INTO public.properties VALUES (1000, 'metalloid', 1.000000, 10, 100);
+INSERT INTO public.properties VALUES (1, 1.008000, -259.1, -252.9, 2);
+INSERT INTO public.properties VALUES (2, 4.002600, -272.2, -269, 2);
+INSERT INTO public.properties VALUES (3, 6.940000, 180.54, 1342, 1);
+INSERT INTO public.properties VALUES (4, 9.012200, 1287, 2470, 1);
+INSERT INTO public.properties VALUES (5, 10.810000, 2075, 4000, 3);
+INSERT INTO public.properties VALUES (6, 12.011000, 3550, 4027, 2);
+INSERT INTO public.properties VALUES (7, 14.007000, -210.1, -195.8, 2);
+INSERT INTO public.properties VALUES (8, 15.999000, -218, -183, 2);
+INSERT INTO public.properties VALUES (1000, 1.000000, 10, 100, 3);
 
 
 --
@@ -212,6 +212,14 @@ ALTER TABLE ONLY public.elements
 
 ALTER TABLE ONLY public.elements
     ADD CONSTRAINT unique_symbol UNIQUE (symbol);
+
+
+--
+-- Name: properties fk_type_id; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.properties
+    ADD CONSTRAINT fk_type_id FOREIGN KEY (type_id) REFERENCES public.types(type_id);
 
 
 --
